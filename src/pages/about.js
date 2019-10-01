dfee114fbc878ce1ebf18c1dc461ccd75b7ad6cd
Oliver Plunkett <otplunkett@gmail.com>
2019-10-01 08:35:45 -0400

    add about and faq

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,34 +10,162 @@ import colors from "../lib/colors"
 export default class AboutPage extends React.Component {
   render() {
     return (
-      <Box>
+      <Box style={{ height: "auto", position: "relative" }}>
         <SEO title="About" />
 
         <HeaderComponent searchProps={{}} siteTitle="Tour Food" />
 
         <Header
-          style={{ textAlign: "center", marginTop: "40px" }}
+          style={{
+            textAlign: "center",
+            marginTop: "40px",
+          }}
           color={colors.blue}
         >
-          about
+          about{" "}
+          <span style={{ fontFamily: "BrandonGrotesqueLight" }}>tour food</span>
         </Header>
 
         <Text
           small
+          color={colors.blue}
           style={{
-            padding: "10px 40px",
+            padding: "25px",
             fontFamily: "BrandonGrotesqueLight",
             maxWidth: "900px",
             margin: "0 auto",
           }}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget
-          feugiat diam. Integer in rhoncus odio. Suspendisse cursus quis tellus
-          vel finibus. Vivamus rutrum mollis interdum. Nullam aliquet a ante a
-          molestie. In interdum odio imperdiet lectus gravida tincidunt. Aenean
-          eu tincidunt mauris, a mattis mauris. Suspendisse tincidunt sed leo in
-          ullamcorper. Cras vestibulum arcu est, id sodales lorem pharetra
-          vitae.
+          Tour Food is a work-in-progress searchable database created to answer
+          every traveler’s favorite daily question: “Where are we going to eat?”
+          <br />
+          <br />
+          What began in 2012 as a Google Spreadsheet compiled by a few musicians
+          passionate about eating well on tour, quickly grew into a privately
+          shared resource featuring contributions from dozens of touring
+          musicians. After years of expansion, we decided it was time to turn
+          “the spreadsheet” into an easily navigable, functional website. We
+          painstakingly checked, sorted, and tagged each listing, cursing
+          whoever added a Chick-Fil-A in Idaho and praising the genius who found
+          the family-owned gas station in Wyoming that serves outstanding Indian
+          food.
+          <br />
+          <br />
+          The refreshed and refined spreadsheet is now the heart and soul of
+          this website: a public, shared space where all types of traveling
+          folks can benefit from the wisdom of musicians whose touring
+          highlights sometimes include more delicious meals than well-attended
+          performances. We only play for about an hour each night. But during
+          the other 23, we’re eating or thinking about eating.
+        </Text>
+
+        <Header
+          style={{ textAlign: "center", marginTop: "25px" }}
+          color={colors.blue}
+        >
+          HOW IS THIS DIFFERENT FROM OTHER FOOD WEBSITES?
+        </Header>
+
+        <Text
+          small
+          color={colors.blue}
+          style={{
+            padding: "25px",
+            fontFamily: "BrandonGrotesqueLight",
+            maxWidth: "900px",
+            margin: "0 auto",
+          }}
+        >
+          Our criteria are simple: quality, value (roughly $20 or less for a
+          meal), and whenever possible, regionality– something that can be
+          elusive on tour. Over years of touring across the country, we have
+          been lucky to experience and appreciate America’s diversity through
+          its food, and want to share our rolodex with anyone interested. Our
+          ever-evolving lists of restaurants, bakeries, and cafes are
+          non-hierarchical: there are no listicles, rankings, or any “best of”
+          content. The information we give you is lean but meaningful. It’s like
+          asking a friend where to eat in {"{"}x{"}"} town and they text back
+          “you should go to {"{"}x{"}"}.” But in this scenario, we’re your
+          friend. Or rather, the many touring musicians who have contributed to
+          this database are your friends. And what makes us different than Joe
+          Schmo on Yelp is that we’ve eaten pizza in enough cities to know
+          whether the best pizza in Norman, Oklahoma is actually worth your time
+          or if there’s something more special worth pursuing. No offense to
+          Joe. Or that pizza place in Norman. Our main goal is functionality:
+          you’re on tour, you’re in a new or unfamiliar city or town, you don’t
+          know anybody there, and you want to know what to eat. You come to this
+          website and realize you’re not alone anymore. Other people have been
+          here before you, and they have places to recommend. You’re going to
+          have a great meal.
+        </Text>
+
+        <Header
+          style={{ textAlign: "center", marginTop: "25px" }}
+          color={colors.blue}
+        >
+          made by
+        </Header>
+
+        <Text
+          small
+          color={colors.blue}
+          style={{
+            padding: "25px",
+            fontFamily: "BrandonGrotesqueLight",
+            maxWidth: "900px",
+            margin: "0 auto",
+          }}
+        >
+          <span style={{ fontSize: "1.2em", fontFamily: "BrandonGrotesque" }}>
+            Luke Pyenson
+          </span>
+          <br />
+          <br />
+          Luke plays drums in the indie rock band Frankie Cosmos and contributes
+          food and travel articles and recipes to publications including Saveur,
+          The Boston Globe, The Washington Post, Edible Boston, and more. He has
+          been touring on-and-off since 2010 and full-time since 2016. He has
+          been writing about food professionally since 2007, and spent formative
+          years working part-time in Boston-area restaurant kitchens. He has an
+          MA in the Anthropology of Travel, Tourism, and Pilgrimage, which is a
+          subject area that directly relates to this website. His favorite
+          listing on Tour Food is Saxapahaw General Store, in Saxapahaw, NC.
+          <br />
+          <br />
+          <br />
+          <span style={{ fontSize: "1.2em", fontFamily: "BrandonGrotesque" }}>
+            Charlie Ferguson
+          </span>
+          <br />
+          <br />
+          Charlie was born and raised in Berkeley, CA, currently lives in
+          Brooklyn, NY and plays drums in the band Hurray For The Riff Raff. He
+          worked for years as a server in NYC restaurants (Glasserie, Flatbush
+          Farm, Martha, The Grocery) until he started touring more full time in
+          2015. Charlie met Adam in 2006 and the two have covered thousands of
+          miles together, touring and eating all across the U.S. in their
+          matching Toyota Sienna minivans. Charlie met Luke in 2019 when Adam
+          realized his friends' mutual love for ice cream and cooking, and
+          thought they would make a good Tour Food team. Charlie loves cooking
+          with immigrant grandmas and his favorite listing on Tour Food is
+          Snow's BBQ in Lexington, TX.
+          <br />
+          <br />
+          <br />
+          <span style={{ fontSize: "1.2em", fontFamily: "BrandonGrotesque" }}>
+            Adam Schatz
+          </span>
+          <br />
+          <br />
+          Adam writes and releases songs with his band Landlady and plays
+          keyboards and saxophone with lots of other fun folks including This Is
+          The Kit and Japanese Breakfast. He produces records for others out of
+          his Ditmas Park recording space, The Chamber Of Commerce. In the
+          kitchen he has fallen off the deep end with baking sourdough bread and
+          a developing signature donut recipe. He has written articles for The
+          Talkhouse and has read articles in Bon Appetit. He pays a monthly fee
+          for The New York Times Cooking. His favorite listing on Tour Food is
+          Perly’s in Richmond, VA.
         </Text>
       </Box>
     )
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import SEO from "../components/seo"
 import HeaderComponent from "../components/header"
@@ -10,7 +11,7 @@ import colors from "../lib/colors"
 export default class FAQPage extends React.Component {
   render() {
     return (
-      <Box>
+      <Box style={{ height: "auto", position: "relative" }}>
         <SEO title="FAQ" />
 
         <HeaderComponent searchProps={{}} siteTitle="Tour Food" />
@@ -24,20 +25,126 @@ export default class FAQPage extends React.Component {
 
         <Text
           small
+          color={colors.blue}
           style={{
-            padding: "10px 40px",
+            padding: "25px",
             fontFamily: "BrandonGrotesqueLight",
             maxWidth: "900px",
             margin: "0 auto",
           }}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget
-          feugiat diam. Integer in rhoncus odio. Suspendisse cursus quis tellus
-          vel finibus. Vivamus rutrum mollis interdum. Nullam aliquet a ante a
-          molestie. In interdum odio imperdiet lectus gravida tincidunt. Aenean
-          eu tincidunt mauris, a mattis mauris. Suspendisse tincidunt sed leo in
-          ullamcorper. Cras vestibulum arcu est, id sodales lorem pharetra
-          vitae.
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            Does the internet really need another food website?
+          </span>
+          <br />
+          No. Wait...Yes.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            Why is this a minimalist website and not an app?
+          </span>
+          <br />
+          We couldn’t afford to pay for app development.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            Is this a complete list of places to eat around the US and Canada?
+          </span>
+          <br />
+          Nope! This is just the beginning, based on everywhere we and our
+          trusted touring musician associates have been to so far.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            I’m not a touring musician, is this site for me?
+          </span>
+          <br />
+          Of course! Our hope is that many different types of travelers will
+          benefit from our database. Some things about this site make it
+          particularly well-suited to musicians, but it is in no way meant to be
+          insular.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            Why isn’t my favorite spot on here?
+          </span>
+          <br />
+          We didn’t know about it! If you believe there is an important place we
+          are missing, please send us a message in the ​Contact Form ​and we
+          will happily consider adding it.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            Does Tour Food have listings that cater to my dietary restriction?
+          </span>
+          <br />
+          There are tons of vegetarian and vegan listings on this site, as well
+          as omnivore restaurants that cater to people with a range of dietary
+          restrictions. Vegan and vegetarian-specific restaurants are tagged as
+          such. Other classifications such as “gluten free” aren’t specifically
+          designated, but rest assured that you’ll find somewhere good to eat,
+          no matter what your diet is.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            How much work are you doing for me?
+          </span>
+          <br />
+          We’ve culled, sorted, and organized our favorite places around the US
+          and Canada for your scrolling pleasure. If a listing piques your
+          interest, follow your heart into the wilds of the Greater Internet in
+          search of more specifics: business hours, menus, and, most
+          importantly, blurry photos of tofu curry. Those are things we can’t
+          and could never provide.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            What determines an 'open late' tag?
+          </span>
+          <br />
+          Open until 11:00pm or later.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            Is everything on tour food within a certain price range?
+          </span>
+          <br />
+          For the most part, we stuck to places where you can eat well for $20
+          or less. There are some places where you may spend more that we’ve
+          included because we think the experience is too special to ignore
+          (e.g. a seafood shack in Maine). In general we have tried to stay
+          budget-conscious, while appreciating that everybody has their own tour
+          budget that works for them.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            Why do some listings have notes?
+          </span>
+          <br />
+          Many places have suggested dishes or other relevant notes, and our
+          hope is to provide those insightful details as much as possible. If a
+          listing doesn’t have any extra commentary, that doesn’t mean it’s
+          bad...we just don’t have as much to say about it. If you eat somewhere
+          and think we should add a note, ​get in touch.
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            Will you add more international listings in the future?
+          </span>
+          <br />
+          We’re planning to!
+          <br />
+          <br />
+          <span style={{ fontFamily: "BrandonGrotesque" }}>
+            I ate at a place you suggested and it stunk!
+          </span>
+          <br />
+          Whoops! These things happen. Feel free to write in the{" "}
+          <Link style={{ color: colors.orange }} to="/contact">
+            Contact Form
+          </Link>{" "}
+          and let us know that one of our recommendations isn’t up to snuff.
+          Don’t hold back.
         </Text>
       </Box>
     )
